4987401bede44617ccf2b9ce7655b8403d861439
ym <geehhee@gmail.com>
2020-05-03 17:29:01 +0900

    text/html対応

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja">');
+  res.write('<body>');
+  res.write('<h1>HTMLの一番大きい見出しを表示します</h1>');
+  res.write('</body>');
+  res.write('</html>');
   res.end();
 });
 const port = 8000;
